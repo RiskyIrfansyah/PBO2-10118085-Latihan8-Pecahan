de3b8b92ec62cfed10bd63aad8737be368394dab
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-09-26 08:05:44 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118085/latihan8/PBO210118085Latihan8.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan8;
+
+/**
+ * @author 
+ * NAMA                 : RISKY IRFRANSYAH
+ * KELAS                : IF-2
+ * NIM                  : 10118085
+ * DESKRIPSI PROGRAM    : Pecahan
+ * @author RISKY
+ */
+public class PBO210118085Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
